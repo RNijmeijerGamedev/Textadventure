cbf34083e38c5e57b390d4679f7aa05e7b1afdf8
Ruben Nijmeijer <ruben@Rubens-MacBook-Pro.local>
2018-06-25 13:39:32 +0200

    VuilSuit added

---
+++ VaultSuit.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace ZuulCS{
+    public class VaultSuit : Item {
+
+        public VaultSuit(){
+            name = "VaultSuit;
+        }
+    }
+
+}
